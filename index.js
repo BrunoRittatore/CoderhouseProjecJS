259c56b636b1502ab24fe462810cb5fa0bc95e37
Bruno Rittatore <brunogro96@hotmail.com>
2022-09-03 21:39:56 -0300

    Simulador Interactivo

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 console.log('Hola mundo');
 
-const hola = 'Hola TUTI';
+function entrega1 (){
+    const hola = 'Hola TUTI';
 let cantidadNotas = parseInt(prompt("Ingrese cantidad de notas a cargar"));
 let totalNotas = 0, num, nota;
 num = 0;
@@ -18,6 +19,21 @@ if (cantidadNotas == 0) {
     alert('El total de las notas es : ' + totalNotas)
     alert('El promedio del total de notas es : ' + (totalNotas / cantidadNotas));
 }
+}
+
+
+function algoritmo (){
+    let n;
+    let capitalDepositado, capitalAcumulado,meses,tasaDeInteres;
+    capitalDepositado = parseInt(document.querySelector('#capital-depositado').value);
+    meses = parseInt(document.querySelector('#duracion-deposito').value);
+    tasaDeInteres = parseInt(document.querySelector('#tasa-de-interes').value);
+    capitalAcumulado = capitalDepositado * Math.pow(1.0+tasaDeInteres/100/365,meses*30)
+    console.log('Capital depositado ' + capitalDepositado + ' acumulado: '+ capitalAcumulado);
+    alert (' El valor acumulado del capital es de : '+capitalAcumulado);
+ 
+}
+
 
 
 
