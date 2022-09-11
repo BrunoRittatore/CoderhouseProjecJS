e6a51dea8133edf2d806632567103827a37db64f
Bruno Rittatore <brunogro96@hotmail.com>
2022-09-10 23:07:44 -0300

    Fix

--- index.js
+++ index.js
@@ -23,13 +23,8 @@ if (cantidadNotas == 0) {
 
 
 function algoritmo (){
-    const variables = {
-        TNA:null,
-        TM:null,
-        capitalAcumulado:null,
-        meses:null,
-        capitalDepositado:null,
-    }
+   
+    variables = [TNA=null,TM=null,capitalAcumulado=null,meses=null,capitalDepositado=null];
         
     variables.capitalDepositado = parseInt(document.querySelector('#capital-depositado').value);
     variables.meses = parseInt(document.querySelector('#duracion-deposito').value);
